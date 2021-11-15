0c1d47e4271666e2f4f4acb6ef334df4484835d2
Gianmarcoascione <91076717+Gianmarcoascione@users.noreply.github.com>
2021-11-15 19:37:25 +0100

    Create esempio-02.py

---
+++ esempio-02.py
@@ -0,0 +1,36 @@
+Python 3.9.7 (tags/v3.9.7:1016ef3, Aug 30 2021, 20:05:41) [MSC v.1929 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+====================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python39-32/esercizio 2.py =====================================================
+12
+>>> 
+====================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python39-32/esercizio 2.py =====================================================
+12
+>>> 
+====================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python39-32/esercizio 2.py =====================================================
+12
+>>> 
+====================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python39-32/esercizio 2.py =====================================================
+20
+>>> 
+====================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python39-32/esercizio 2.py =====================================================
+12
+>>> 
+====================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python39-32/esercizio 2.py =====================================================
+30
+>>> 
+====================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python39-32/esercizio 2.py =====================================================
+30
+>>> 
+====================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python39-32/esercizio 2.py =====================================================
+30
+>>> 
+====================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python39-32/esercizio 2.py =====================================================
+80
+>>> 
+====================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python39-32/esercizio 2.py =====================================================
+80
+>>> 
+====================================================== RESTART: C:/Users/DELL/AppData/Local/Programs/Python/Python39-32/esercizio 2.py =====================================================
+-80
+>>> 
