5a6d7326b4cc3ca4da19fe4f9bd6b94a77fd672a
Gianmarcoascione <91076717+Gianmarcoascione@users.noreply.github.com>
2022-10-24 18:40:44 +0200

    Add files via upload

---
+++ esempio2-3.py
@@ -0,0 +1,10 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+a=0o9
+SyntaxError: invalid digit '9' in octal literal
+A=0o7
+print (A)
+7
+C=0b1100
+print(C)
+12
